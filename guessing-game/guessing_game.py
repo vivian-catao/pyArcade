c24d47819d65eb21c8abb9db032b3dbe1845a39f
Vivian Tenório <vivian.tenorio@virtus.ufcg.edu.br>
2022-12-06 22:35:34 -0300

    Implementation for one wrong guess

--- guessing-game/guessing_game.py
+++ guessing-game/guessing_game.py
@@ -1,3 +1,7 @@
 print("*********************************")
 print("**Welcome to the Guessing Game!**")
 print("*********************************")
+
+def divination(n_secret, guess):
+    if (n_secret != guess):
+        return False

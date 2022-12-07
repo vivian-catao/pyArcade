4138b241cf7bb1404c0be666bd4ddfe61778ebc4
Vivian Tenório <vivian.tenorio@virtus.ufcg.edu.br>
2022-12-06 22:11:06 -0300

    Added guessing-game test file

---
+++ guessing-game/test_guessing_game.py
@@ -0,0 +1,16 @@
+import unittest
+from guessing_game import *
+
+class Test_guessing_game_parameters(unittest.TestCase):
+    def test_default_is_valid(self):
+        self.assertEqual(1, 0)
+
+#class Test_guessing-game_behaviour(unittest.TestCase):
+    #def setUp(self):
+        #
+
+    #def test_(self):
+    #
+
+if __name__=='__main__':
+    unittest.main()

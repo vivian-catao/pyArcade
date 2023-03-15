fb1ce8e9b26351a81298e838f3ba91eb10a8b110
Vivian Tenório <vivian.tenorio@virtus.ufcg.edu.br>
2023-03-14 22:10:26 -0300

    [FEAT] Terminal Interaction

--- guessing-game/guessing_game.py
+++ guessing-game/guessing_game.py
@@ -1,3 +1,5 @@
+import random
+
 print("*********************************")
 print("**Welcome to the Guessing Game!**")
 print("*********************************")
@@ -9,3 +11,22 @@ def divination(n_secret, guess):
         elif guess < n_secret:
             return 'higher'
     else: return 'Congrats'
+    
+if __name__ == '__main__':
+    
+    total_tentativas = 5
+    n_secret = random.randrange(0, 100)
+    
+    for rodada in range (1, total_tentativas + 1):
+        print("Tentativa {} de {}" .format(rodada, total_tentativas))
+        guess = input("Digite um palpite entre 1 e 100: {}    ".format(n_secret))
+        if (int(guess) < 1 or int(guess) > 100):
+            print("Você deve digitar um número de 0 a 100")
+            continue 
+        if divination(n_secret, int(guess)) == 'lower':
+            print("Você digitou {}, the secret number is lower" .format(guess))
+        elif divination(n_secret, int(guess)) == 'higher':
+            print("Você digitou {}, the secret number is higher".format(guess))
+        elif divination(n_secret, int(guess)) == 'Congrats':
+            print("Congrats, Você ganhou")
+            break
--- guessing-game/test_guessing_game.py
+++ guessing-game/test_guessing_game.py
@@ -13,12 +13,10 @@ class Test_guessing_game_behaviour(unittest.TestCase):
     def test_if_the_player_is_wrong_returns_higher_or_lower(self):
         for guess in range(1, 50):
             n_secret = 51
-            print(guess)
             self.assertEqual('higher', divination(n_secret, guess))
         
         for guess in range(51,100):
             n_secret = 50
-            print(guess)
             self.assertEqual('lower', divination(n_secret, guess))
 
 if __name__=='__main__':

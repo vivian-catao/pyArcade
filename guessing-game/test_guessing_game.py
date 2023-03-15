00fe573ea326c6f29a847239d201206ac50e3cf7
Vivian Tenório <vivian.tenorio@virtus.ufcg.edu.br>
2023-03-14 21:38:31 -0300

    [REFACTOR] For in high or lower test

--- guessing-game/test_guessing_game.py
+++ guessing-game/test_guessing_game.py
@@ -1,10 +1,6 @@
 import unittest
 from guessing_game import *
 
-class Test_guessing_game_parameters(unittest.TestCase):
-    def test_default_is_valid(self):
-        self.assertEqual(1, 1)
-
 class Test_guessing_game_behaviour(unittest.TestCase):
     #def setUp(self):
     #    pass
@@ -15,12 +11,15 @@ class Test_guessing_game_behaviour(unittest.TestCase):
         self.assertEqual('Congrats', divination(n_secret, guess))
     
     def test_if_the_player_is_wrong_returns_higher_or_lower(self):
-        n_secret = 30
-        guess = 20
-        self.assertEqual('higher', divination(n_secret, guess))
-        n_secret = 30
-        guess = 50
-        self.assertEqual('lower', divination(n_secret, guess))
+        for guess in range(1, 50):
+            n_secret = 51
+            print(guess)
+            self.assertEqual('higher', divination(n_secret, guess))
+        
+        for guess in range(51,100):
+            n_secret = 50
+            print(guess)
+            self.assertEqual('lower', divination(n_secret, guess))
 
 if __name__=='__main__':
     unittest.main()
